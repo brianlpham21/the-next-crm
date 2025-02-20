b149c1b7b3cb76cf10740d8e2930dea1f97c2157
Brian Pham <brian@kahaniapp.com>
2025-02-20 10:24:43 -0800

    retrigger github actions for deploy to github pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,7 @@
-import Image from "next/image";
-
 export default function Dashboard() {
   return (
     <div>
       <main>Dashboard</main>
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
     </div>
   );
 }
